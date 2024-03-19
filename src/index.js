9e204d1a4f205c3535e0eee795c615b688e9a7da
Alex Clark <aclark@aclark.net>
2024-03-19 13:37:24 -0400

    Update pillow-aswf

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
-// import 'reveal.js/plugin/notes/notes.js';
 import Reveal from 'reveal.js';
-Reveal.initialize();
+import RevealNotes from 'reveal.js/plugin/notes/notes.js';
+Reveal.initialize({ plugins: [ RevealNotes ]});
